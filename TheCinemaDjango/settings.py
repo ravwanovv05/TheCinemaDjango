8e006b84fdbf22a5ca5c35871b29d5f7f2c0afbc
Samidillo Ravshanov <samidilloravshanov14@gmail.com>
2024-11-12 03:28:38 +0500

    deleted migrations

--- TheCinemaDjango/settings.py
+++ TheCinemaDjango/settings.py
@@ -101,6 +101,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
